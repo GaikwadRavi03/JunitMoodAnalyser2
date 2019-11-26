308ae006ab0cc0517de2f8276f5920c0259c7c77
GaikwadRavi03 <gaikwadr576@gmail.com>
2019-11-26 10:13:39 -0500

    Add: Message return sad

--- src/main/java/com/bridgelabz/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -1,4 +1,10 @@
 package com.bridgelabz;
 
 public class MoodAnalyser {
+
+    private String message;
+
+    public String analyseMood(String message) {
+        return "SAD";
+    }
 }
--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -1,4 +1,14 @@
 package com.bridgelabz;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class MoodAnalyserTest {
+
+    @Test
+    public void givenMessageWhenSadShow() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("This is SAD message");
+        Assert.assertEquals("SAD", mood);
+    }
 }
